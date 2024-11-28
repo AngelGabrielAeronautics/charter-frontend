97fa1b65f7ee31b178c9709e6ba978aadb582609
Arthurnatious Somdaka <148981447+givensomdaka@users.noreply.github.com>
2024-11-28 23:02:52 +0200

    Operator Assets update as in Admin Assets

--- app/(protected)/operator/assets/page.tsx
+++ app/(protected)/operator/assets/page.tsx
@@ -2,16 +2,24 @@
 
 import React, { useEffect, useState } from "react";
 
-import { Divider, TableColumnType } from "antd";
+
+
+import { Descriptions, Divider, TableColumnType } from "antd";
+
+
 
 import DataTable from "@/app/components/DataTable";
 import CreateAssetsDrawer from "@/app/components/Drawers/CreateAssetDrawer";
 import PageHeader from "@/app/components/PageHeader";
 import EditAssetDrawer from "@/app/components/editAssetDrawer";
 
+
+
 import { IAsset } from "@/lib/models/IAssets";
 import { filter } from "@/lib/state/assets/assets.slice";
 import { useAppDispatch, useAppSelector } from "@/lib/state/hooks";
+import dayjs from "dayjs";
+
 
 const Assets = () => {
   const { authenticatedUser } = useAppSelector((state) => state.auth);
@@ -43,18 +51,87 @@ const Assets = () => {
 
   const columns: TableColumnType<any>[] = [
     {
-      title: "Baggage Compartment Size",
-      key: "baggageCompartmentSize",
-      dataIndex: "baggageCompartmentSize",
-      render: (value: number[]) => `${value[0]}m x ${value[1]}m x ${value[2]}m`,
+      title: "Cruise Speed (Knots)",
+      key: "cruiseSpeedInKnots",
+      dataIndex: "cruiseSpeedInKnots",
+      render: (value) => {
+        return value;
+      },
+    },
+    {
+      title: "Registration Number",
+      key: "registrationNumber",
+      dataIndex: "registrationNumber",
+      render: (value) => {
+        return value;
+      },
     },
     {
       title: "Year of Manufacture",
       key: "yearOfManufacture",
       dataIndex: "yearOfManufacture",
-      render: (value: number) => value,
+      render: (value) => {
+        return value;
+      },
+    },
+    {
+      title: "Last Refurbishment Date",
+      key: "lastRefurbishmentDate",
+      dataIndex: "lastRefurbishmentDate",
+      render: (value) => {
+        return dayjs(value).format("DD MMM YYYY");
+      },
+    },
+    {
+      title: "Cargo Capacity",
+      key: "cargoCapacity",
+      dataIndex: "cargoCapacity",
+      render: (value, record) => {
+        return `${value ?? "--"} ${record.cargoCapacityUnits ?? ""}`;
+      },
+    },
+    {
+      title: "Baggage Compartment Max Weight",
+      key: "baggage_compartment_max_weight",
+      dataIndex: "baggage_compartment_max_weight",
+      render: (value, record) => {
+        return `${value} ${record.baggageCompartmentMaxWeightUnits}`;
+      },
+    },
+    {
+      title: "Baggage Compartment Size",
+      key: "baggageCompartmentSize",
+      dataIndex: "baggageCompartmentSize",
+      render: (value) => {
+        return (
+          <Descriptions size="small" bordered column={3} layout="vertical">
+            <Descriptions.Item
+              label="L"
+              labelStyle={{ fontWeight: "bold" }}
+              span={1}
+            >
+              {value[0]}m
+            </Descriptions.Item>
+            <Descriptions.Item
+              label="W"
+              labelStyle={{ fontWeight: "bold" }}
+              span={1}
+            >
+              {value[1]}m
+            </Descriptions.Item>
+            <Descriptions.Item
+              label="H"
+              labelStyle={{ fontWeight: "bold" }}
+              span={1}
+            >
+              {value[2]}m
+            </Descriptions.Item>
+          </Descriptions>
+        );
+      },
     },
   ];
+
   const createDrawerConfig = {
     isOpen: createDrawerOpen,
     openDrawer: () => {
@@ -104,6 +181,7 @@ const Assets = () => {
         customCreateDrawer={createDrawerConfig}
         customColumns={columns}
         onSelectRow={handleEditAsset}
+        hiddenColumns={["operatorId", "baggageCompartmentMaxWeightUnits", "cargoCapacityUnits"]}
       />
     </div>
   );
