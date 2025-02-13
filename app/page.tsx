ec71f6e827c8af04af976b170583a80e2ba026af
Thato Lebethe <thatofreelance@gmail.com>
2025-02-13 10:27:39 +0200

    left aligned promo text with heading

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,19 @@ export default function Home() {
       <div className="relative z-10 w-full">
         <ClientAppBar />
         <div className="mb-28 mr-28 mt-32 flex h-full min-h-full flex-col items-end">
-          <h1 style={{ color: "#F9EFE4" }} className="text-right">
-            LAUNCHING 2025!
-          </h1>
-          <p
-            style={{ color: "#F9EFE4", fontWeight: "400" }}
-            className="text-right font-montserrat text-xl font-light leading-relaxed"
-          >
-            Compare quotes from <br />
-            multiple charter operators <br />
-            with one simple request.
-          </p>
+          <div>
+            <h1 style={{ color: "#F9EFE4" }} className="text-left">
+              LAUNCHING 2025!
+            </h1>
+            <p
+              style={{ color: "#F9EFE4", fontWeight: "400" }}
+              className="text-left font-montserrat text-xl font-light leading-relaxed"
+            >
+              Compare quotes from
+              <br /> multiple charter operators with one
+              <br /> simple request.
+            </p>
+          </div>
         </div>
         <FlightSearch />
       </div>
